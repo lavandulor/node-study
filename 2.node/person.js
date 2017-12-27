dfe1ac21ad2831969decca54fee4394763eb8820
hjw <505575616@qq.com>
2017-12-27 16:21:40 +0800

    修改目录结构

---
+++ 2.node/person.js
@@ -0,0 +1,8 @@
+/**
+ * Created by hjw on 2017/12/27.
+ */
+var name = 'hjw';
+var age = 25;
+
+exports.name = name;
+exports.age = age;
---
+++ 2.node/userP.js
@@ -0,0 +1,6 @@
+/**
+ * Created by hjw on 2017/12/27.
+ */
+var person = require('./person');
+console.log(person.name);
+console.log(person.age);
